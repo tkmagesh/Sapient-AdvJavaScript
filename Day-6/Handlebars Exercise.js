3b2ecef529e683ffc8256a4151029590da7030c9
tkmagesh <tkmagesh77@gmail.com>
2013-10-03 11:32:38 -0700

    HandlebarsJs Exercise

---
+++ Day-6/Handlebars Exercise.js
@@ -0,0 +1,10 @@
+var books = [
+		{id:1,title:"JavaScript in a Nutshell",cost:12,inStock:true},
+		{id:2,title:"JavaScript Design Patterns",cost:24,inStock:false},
+		{id:3,title:"JavaScript: The good parts",cost:15,inStock:true},
+		{id:4,title:"Secrets of a JavaScript Ninja",cost:28,inStock:true},
+		{id:5,title:"Illustrated JavaScript",cost:11,inStock:false},
+		{id:6,title:"JavaScript and the Web",cost:16,inStock:true}
+	];
+
+
